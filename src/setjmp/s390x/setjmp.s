66fac997e3058eab1a03a891d5018603126bb06f
Bobby Bingham <koorogi@koorogi.info>
2016-06-21 23:16:41 -0500

    s390x: fix return value of setjmp

--- src/setjmp/s390x/setjmp.s
+++ src/setjmp/s390x/setjmp.s
@@ -12,13 +12,14 @@ _setjmp:
 setjmp:
 	stmg %r6, %r15, 0(%r2)
 
-	std  %f8, 10*8(%r2)
-	std  %f9, 11*8(%r2)
-	std %f10, 12*8(%r2)
-	std %f11, 13*8(%r2)
-	std %f12, 14*8(%r2)
-	std %f13, 15*8(%r2)
-	std %f14, 16*8(%r2)
-	std %f15, 17*8(%r2)
+	std  %f8,  10*8(%r2)
+	std  %f9,  11*8(%r2)
+	std  %f10, 12*8(%r2)
+	std  %f11, 13*8(%r2)
+	std  %f12, 14*8(%r2)
+	std  %f13, 15*8(%r2)
+	std  %f14, 16*8(%r2)
+	std  %f15, 17*8(%r2)
 
-	br  %r14
+	lghi %r2, 0
+	br   %r14
